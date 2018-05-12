a0ccc5d12ec926503a8269382fb3f4657c97b260
kevin wilkinson <willkillson@gmail.com>
2018-05-11 21:58:32 -0700

    6/16 tests passing using basic regex

--- src/com/cft/contactmerge/io/SplitStringUtilities.java
+++ src/com/cft/contactmerge/io/SplitStringUtilities.java
@@ -1,17 +1,44 @@
 package com.cft.contactmerge.io;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.*;
 
 public class SplitStringUtilities {
     public static List<String> splitTsvString(String input)
     {
-        // TODO: Add code to return appropriate results
-        return null;
+        List<String> x = new ArrayList<String>();
+
+        String testString = input;
+        String _regex = "[\t]";
+
+        String[] _split = testString.split(_regex);
+
+        for(int i = 0;i< _split.length;i++){
+            x.add(_split[i]);
+        }
+
+
+
+
+        return x;
     }
 
     public static List<String> splitCsvString(String input)
     {
-        // TODO: Add code to return appropriate results
-        return null;
+        List<String> x = new ArrayList<String>();
+
+        String testString = input;
+        String _regex = "[,]";
+
+        String[] _split = testString.split(_regex);
+
+        for(int i = 0;i< _split.length;i++){
+            x.add(_split[i]);
+        }
+
+        return x;
+
     }
 }
