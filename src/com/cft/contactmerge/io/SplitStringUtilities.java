c20ad4e52b4795c0a2ec0ff6690b0d0033c47a25
kevin wilkinson <willkillson@gmail.com>
2018-05-13 20:37:38 -0700

    10/16 tests pass, minor copying code from TSV to CSV with replacing tab with a comma

--- src/com/cft/contactmerge/io/SplitStringUtilities.java
+++ src/com/cft/contactmerge/io/SplitStringUtilities.java
@@ -65,14 +65,50 @@ public class SplitStringUtilities {
         List<String> x = new ArrayList<String>();
 
         String testString = input;
-        String _regex = "[,]";
 
-        String[] _split = testString.split(_regex);
+        String _string = new String("");
+        for(int i = 0;i< testString.length();i++){
+            if(input.charAt(i)==','){
+                x.add(_string);
+                _string = "";
+            }
+            else {
+                _string= _string+input.charAt(i);
+            }
+        }
+        x.add(_string);
+        //test
+        //////////Remove white space before any character, and after the last character in the strings.
 
-        for(int i = 0;i< _split.length;i++){
-            x.add(_split[i]);
+        List<String> y = new ArrayList<String>();
+
+        for(ListIterator itb = x.listIterator();itb.hasNext();){
+            String strToCheck = itb.next().toString();
+
+            for(int j = 0;j<2;j++) {
+                String wsf = "";
+                boolean iwsf = true;
+
+
+                for (int i = 0; i < strToCheck.length(); i++) {
+                    if (strToCheck.charAt(i) == ' ') {
+
+                    } else {
+                        iwsf = false;
+                    }
+
+                    if (iwsf == false) {
+                        wsf += strToCheck.charAt(i);
+                    }
+                }
+                strToCheck = wsf;
+                StringBuilder strb = new StringBuilder(strToCheck);
+
+                strToCheck = strb.reverse().toString();
+            }
+            y.add(strToCheck);
         }
 
-        return x;
+        return y;
     }
 }
