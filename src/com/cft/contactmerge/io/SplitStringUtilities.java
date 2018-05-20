8efbd92b4da9806e96d94a391aa7629087fad028
Michael <mbilodeau6@hotmail.com>
2018-05-20 12:26:51 -0700

    Implemented SplitStringUtilities to pass tests and updated tests.

--- src/com/cft/contactmerge/io/SplitStringUtilities.java
+++ src/com/cft/contactmerge/io/SplitStringUtilities.java
@@ -1,17 +1,35 @@
 package com.cft.contactmerge.io;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SplitStringUtilities {
-    public static Collection<String> splitTsvString(String input)
+    public static List<String> splitTsvString(String input)
     {
-        // TODO: Add code to return appropriate results
-        return null;
+        List<String> parts = Arrays.asList(input.split("\t", -1));
+
+        return parts.stream().map(x -> x.trim()).collect(Collectors.toList());
     }
 
-    public static Collection<String> splitCsvString(String input)
+    public static List<String> splitCsvString(String input)
     {
-        // TODO: Add code to return appropriate results
-        return null;
+        // from https://stackoverflow.com/questions/15738918/splitting-a-csv-file-with-quotes-as-text-delimiter-using-string-split
+        List<String> parts = Arrays.asList(input.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1))
+                .stream().map(x -> x.trim()).collect(Collectors.toList());
+
+        List<String> newParts = new ArrayList<String>();
+
+        // TODO: Feels like I should be able to do this with a lambda function but have failed to find one that works.
+        for (String part: parts) {
+            if (part.startsWith("\"") && part.endsWith("\"")) {
+                // adapted from https://stackoverflow.com/questions/2088037/trim-characters-in-java
+                newParts.add(part.replaceAll("\"$|^\"", "").trim());
+            }
+            else {
+                newParts.add(part.trim());
+            }
+        }
+
+        return newParts;
     }
 }
--- src/com/cft/contactmerge/io/tests/SplitStringUtilitiesTest.java
+++ src/com/cft/contactmerge/io/tests/SplitStringUtilitiesTest.java
@@ -3,16 +3,198 @@ package com.cft.contactmerge.io.tests;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.cft.contactmerge.io.*;
+import java.util.*;
 
 class SplitStringUtilitiesTest {
 
+    private String createVerificationMessage(String itemName)
+    {
+        return String.format("Verify %s", itemName);
+    }
+
+    /* ----------------------------------------------------------
+     * TSV Tests
+     * ---------------------------------------------------------- */
     @Test
-    void splitTsvString() {
-        // TODO: Add tests
+    void splitTsvString_EmptyString() {
+        List<String> parts = SplitStringUtilities.splitTsvString("");
+
+        assertEquals(1, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "", parts.get(0), createVerificationMessage("first result"));
     }
 
     @Test
-    void splitCsvString() {
-        // TODO: Add tests
+    void splitTsvString_SingleValue() {
+        List<String> parts = SplitStringUtilities.splitTsvString("Joe");
+
+        assertEquals(1, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "first result");
+    }
+
+    @Test
+    void splitTsvString_MultipleValues() {
+        List<String> expectedParts = Arrays.asList("Joe", "Smith", "123 Main St");
+        String testString = String.join("\t", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitTsvString(testString);
+
+        assertEquals(3, actualParts.size(), createVerificationMessage("result size"));
+
+        for(int i=0; i < actualParts.size(); i++) {
+            assertEquals(expectedParts.get(i), actualParts.get(i), String.format("result[%d]", i));
+        }
+    }
+
+    @Test
+    void splitTsvString_MissingValues() {
+        List<String> expectedParts = Arrays.asList("", "Smith", "", "");
+        String testString = String.join("\t", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitTsvString(testString);
+
+        assertEquals(4, actualParts.size(), createVerificationMessage("result size"));
+
+        for(int i=0; i < actualParts.size(); i++) {
+            assertEquals(expectedParts.get(i), actualParts.get(i), String.format("result[%d]", i));
+        }
+    }
+
+    @Test
+    void splitTsvString_LeadingTrailingSpacesTrimmed() {
+        List<String> expectedParts = Arrays.asList("   ", " Smith  ");
+        String testString = String.join("\t", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitTsvString(testString);
+
+        assertEquals(2, actualParts.size(), createVerificationMessage("result size"));
+
+        assertEquals("", actualParts.get(0), "result[0]");
+        assertEquals("Smith", actualParts.get(1), "result[1]");
+    }
+
+    /* ----------------------------------------------------------
+     * CSV Tests
+     * ---------------------------------------------------------- */
+    @Test
+    void splitCsvString_EmptyString() {
+        List<String> parts = SplitStringUtilities.splitCsvString("");
+
+        assertEquals(1, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "", parts.get(0), createVerificationMessage("first result"));
+    }
+
+    @Test
+    void splitCsvString_SingleValue() {
+        List<String> parts = SplitStringUtilities.splitCsvString("Joe");
+
+        assertEquals(1, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "first result");
+    }
+
+    @Test
+    void splitCsvString_MultipleValues() {
+        List<String> expectedParts = Arrays.asList("Joe", "Smith", "123 Main St");
+        String testString = String.join(",", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitCsvString(testString);
+
+        assertEquals(3, actualParts.size(), createVerificationMessage("result size"));
+
+        for(int i=0; i < actualParts.size(); i++) {
+            assertEquals(expectedParts.get(i), actualParts.get(i), String.format("result[%d]", i));
+        }
+    }
+
+    @Test
+    void splitCsvString_MissingValues() {
+        List<String> expectedParts = Arrays.asList("", "Smith", "", "");
+        String testString = String.join(",", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitCsvString(testString);
+
+        assertEquals(4, actualParts.size(), createVerificationMessage("result size"));
+
+        for(int i=0; i < actualParts.size(); i++) {
+            assertEquals(expectedParts.get(i), actualParts.get(i), String.format("result[%d]", i));
+        }
+    }
+
+    @Test
+    void splitCsvString_LeadingTrailingSpacesTrimmed() {
+        List<String> expectedParts = Arrays.asList("   ", " Smith  ");
+        String testString = String.join(",", expectedParts);
+
+        List<String> actualParts = SplitStringUtilities.splitCsvString(testString);
+
+        assertEquals(2, actualParts.size(), createVerificationMessage("result size"));
+        assertEquals("", actualParts.get(0), "result[0]");
+        assertEquals("Smith", actualParts.get(1), "result[1]");
+    }
+
+    @Test
+    void splitCsvString_SingleQuotedValue() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Smith, Joe\"");
+
+        assertEquals(1, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Smith, Joe", parts.get(0), "first result");
+    }
+
+    @Test
+    void splitCsvString_MixQuotedNonQuoted() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Joe\", Smith, \"123 Main St, Apt 10\"");
+
+        assertEquals(3, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "result[0]");
+        assertEquals( "Smith", parts.get(1), "result[1]");
+        assertEquals( "123 Main St, Apt 10", parts.get(2), "result[2]");
+    }
+
+    @Test
+    void splitCsvString_MixQuotedNonQuotedWithBlanks() {
+        List<String> parts = SplitStringUtilities.splitCsvString(",\"\",\"Joe\", Smith,");
+
+        assertEquals(5, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(2), "result[2]");
+        assertEquals( "Smith", parts.get(3), "result[3]");
+    }
+
+    @Test
+    void splitCsvString_TextAfterQuoted() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Joe\", \"Adam\"son, \"123 Main St\"");
+
+        assertEquals(3, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "result[0]");
+        assertEquals( "\"Adam\"son", parts.get(1), "result[1]");
+        assertEquals( "123 Main St", parts.get(2), "result[2]");
+    }
+
+    @Test
+    void splitCsvString_TextAfterLastQuoted() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Joe\", \"Adam\"son, \"123 Main St\"reet");
+
+        assertEquals(3, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "result[0]");
+        assertEquals( "\"Adam\"son", parts.get(1), "result[1]");
+        assertEquals( "\"123 Main St\"reet", parts.get(2), "result[2]");
+    }
+
+    @Test
+    void splitCsvString_TextBeforeQuoted() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Joe\", Adam\"son\", \"123 Main St\"");
+
+        assertEquals(3, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "result[0]");
+        assertEquals( "Adam\"son\"", parts.get(1), "result[1]");
+        assertEquals( "123 Main St", parts.get(2), "result[2]");
+    }
+
+    @Test
+    void splitCsvString_TextBeforeQuotedWithComma() {
+        List<String> parts = SplitStringUtilities.splitCsvString("\"Joe\", Adam\"s,on\", \"123 Main St\"");
+
+        assertEquals(3, parts.size(), createVerificationMessage("result size"));
+        assertEquals( "Joe", parts.get(0), "result[0]");
+        assertEquals( "Adam\"s,on\"", parts.get(1), "result[1]");
+        assertEquals( "123 Main St", parts.get(2), "result[2]");
     }
 }
