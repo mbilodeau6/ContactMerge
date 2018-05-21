3c7593c60d4484cd7e5881bd9f8bbc32ce199d8f
Michael <mbilodeau6@hotmail.com>
2018-05-20 22:11:57 -0700

    Implemented CSV Importer

--- src/com/cft/contactmerge/io/CsvImporter.java
+++ src/com/cft/contactmerge/io/CsvImporter.java
@@ -1,31 +1,112 @@
 package com.cft.contactmerge.io;
 
-import com.cft.contactmerge.contact.Contact;
+import com.cft.contactmerge.contact.*;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 
 public class CsvImporter implements IImporter, Iterable<Contact> {
 
-    public void Load(String filename)
-    {
-        // TODO: Add code
+    private List<String> data = null;
+    private Map<String, Integer> columnMap;
+
+    public void Load(String filename) throws FileNotFoundException, IOException {
+        // TODO: This method may be the same in all Importer classes. Create parent class?
+        if (filename == null || filename.trim() == "")
+        {
+            throw new IllegalArgumentException("filename is required");
+        }
+
+        File inputFile = new File(filename);
+
+        if (!inputFile.exists() || inputFile.isDirectory())
+        {
+            throw new FileNotFoundException(String.format("Unable to find %s", filename));
+        }
+
+        try (InputStream inputStream = new FileInputStream(inputFile)) {
+            Load(inputStream);
+        }
     }
 
-    public Iterator<Contact> iterator()
-    {
+    private Map<String, Integer> getColumnMap(String headerLine) {
+        Map<String, Integer> columnMap = new Hashtable<String, Integer>();
+
+        List<String> columns = SplitStringUtilities.splitCsvString(headerLine);
+
+        int i = 0;
+        for (String column: columns) {
+            columnMap.put(column, i++);
+        }
+
+        return columnMap;
+    }
+
+    public void Load(InputStream inputStream) {
+        if (inputStream == null)
+        {
+            throw new IllegalArgumentException("inputStream is required");
+        }
+
+        try {
+            if (!(inputStream.available() > 0))
+                throw new UnknownFormatConversionException("inputStream not available");
+        }
+        catch (IOException e)
+        {
+            throw new UnknownFormatConversionException("inputStream not available");
+        }
+
+        // Load header line and data
+        try (Scanner scanner = new Scanner(inputStream)) {
+            if (!scanner.hasNextLine()) {
+                throw new UnknownFormatConversionException("inputStream not available");
+            }
+
+            this.columnMap = getColumnMap(scanner.nextLine());
+
+            data = new ArrayList<String>();
+
+            while(scanner.hasNextLine()) {
+                data.add(scanner.nextLine());
+            }
+        }
+    }
+
+    public Iterator<Contact> iterator() {
+        if (data == null)
+        {
+            throw new IllegalStateException("Must load file before using the iterator");
+        }
+
+
         Iterator<Contact> it = new Iterator<Contact>() {
+            int currentIndex = 0;
 
             @Override
             public boolean hasNext() {
-
-                // TODO: Add code and return appropriate result
-                return false;
+                return currentIndex < data.size();
             }
 
             @Override
             public Contact next() {
                 Contact contact = new Contact();
 
-                // TODO: Fill in code
+                List<String> parts = SplitStringUtilities.splitCsvString(data.get(currentIndex++));
+
+                contact.setName(new Name(new LastName(parts.get(columnMap.get("Last"))),
+                        new FirstName(parts.get(columnMap.get("First")))));
+
+                contact.setAddress(new Address(new StreetAddress(parts.get(columnMap.get("Address"))),
+                        new GeneralProperty(parts.get(columnMap.get("City"))),
+                        new GeneralProperty(parts.get(columnMap.get("State"))),
+                        null,
+                        new GeneralProperty(parts.get(columnMap.get("Zip")))));
+
+                contact.setPhone(new PhoneNumber(parts.get(columnMap.get("Phone #"))));
+
+                contact.setEmail(new GeneralProperty(parts.get(columnMap.get("E-mail"))));
 
                 return contact;
             }
--- src/com/cft/contactmerge/io/tests/CsvImporterTest.java
+++ src/com/cft/contactmerge/io/tests/CsvImporterTest.java
@@ -1,18 +1,133 @@
 package com.cft.contactmerge.io.tests;
 
+import com.cft.contactmerge.contact.Contact;
+import com.cft.contactmerge.io.CsvImporter;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UnknownFormatConversionException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CsvImporterTest {
 
+    /* --------------------------------------------------------------------------------
+     * Testing Load(filename)
+     * -------------------------------------------------------------------------------- */
     @Test
-    void CsvImporter_Load() {
-        // TODO: Add tests
+    void Load_NullFileName() {
+        CsvImporter importer = new CsvImporter();
+
+        assertThrows(IllegalArgumentException.class, () -> importer.Load( (String) null));
     }
 
     @Test
-    void CsvImporter_Iterator() {
-        // TODO: Add tests
+    void Load_EmptyFileName() {
+        CsvImporter importer = new CsvImporter();
+
+        assertThrows(IllegalArgumentException.class, () -> importer.Load(""));
+    }
+
+    @Test
+    void Load_NonExistentFileName() {
+        // TODO: Would be great if we could test this without access the file system but articles
+        // I've read indicate you shouldn't try to mock File. How else to do it?
+        CsvImporter importer = new CsvImporter();
+
+        assertThrows(FileNotFoundException.class, () -> importer.Load("c:\\DoesNotExist\\DoesNotExist.xml"));
+    }
+
+    /* --------------------------------------------------------------------------------
+     * Testing Load(inputStream)
+     * -------------------------------------------------------------------------------- */
+    @Test
+    void Load_NullStream() {
+        CsvImporter importer = new CsvImporter();
+
+        assertThrows(IllegalArgumentException.class, () -> importer.Load((InputStream) null));
+    }
+
+    @Test
+    void Load_EmptyStream() throws IOException {
+        CsvImporter importer = new CsvImporter();
+
+        String testString = "";
+
+        try (InputStream inputStream = new ByteArrayInputStream(testString.getBytes())) {
+            assertThrows(UnknownFormatConversionException.class, () -> importer.Load(inputStream));
+        }
+    }
+
+    /* --------------------------------------------------------------------------------
+     * Testing Iterator
+     * -------------------------------------------------------------------------------- */
+
+    private static String testCsv = "Bidder #,CRM ID,Type,Salutation,First,Last,Company,Phone #,E-mail,Address,City,State,Zip,Country,User Defined 1,User Defined 2,Notes,VIP,Table #,Table Name,Ticket #,Ticket Package,Express Checkout Credit Card,RSVP Status,Checked In\n" +
+            "5,,Person,,John,Doe,,(520) 123-4567,jdoe@gmail.com,123 Main St,Tucson,Arizona,85716,,,Beider,,No,18,Group 1,3,Comp Ticket-Honoree,,,Yes\n" +
+            "5,,Person,,Adam,Smith,,(520) 111-2222,adam.smith@yahoo.com,1010 Speedway Blvd,Tucson,AZ,85716,,,,,No,18,Group 1,1,Comp Ticket-Honoree,,,Yes\n";
+
+    @Test
+    void IteratorCalledBeforeLoad() {
+        CsvImporter importer = new CsvImporter();
+
+        assertThrows(IllegalStateException.class, () -> importer.iterator().hasNext());
+    }
+
+    @Test
+    void Load_EmptyDataSet() throws IOException {
+        CsvImporter importer = new CsvImporter();
+
+        String testString = " ";
+
+        try (InputStream inputStream = new ByteArrayInputStream(testString.getBytes())) {
+            importer.Load(inputStream);
+        }
+
+        assertFalse(importer.iterator().hasNext());
+    }
+
+    @Test
+    void IteratorHasNext() throws IOException {
+        CsvImporter importer = new CsvImporter();
+
+        try (InputStream inputStream = new ByteArrayInputStream(testCsv.getBytes())) {
+            importer.Load(inputStream);
+        }
+
+        assertTrue(importer.iterator().hasNext());
+    }
+
+    @Test
+    void IteratorReturnsExpectedData() throws IOException {
+        CsvImporter importer = new CsvImporter();
+
+        try (InputStream inputStream = new ByteArrayInputStream(testCsv.getBytes())) {
+            importer.Load(inputStream);
+        }
+
+        ArrayList<String> lastNames = new ArrayList<String>(Arrays.asList("Doe", "Smith"));
+        ArrayList<String> firstNames = new ArrayList<String>(Arrays.asList("John", "Adam"));
+        ArrayList<String> addresses = new ArrayList<String>(Arrays.asList("123 Main St", "1010 Speedway Blvd"));
+        ArrayList<String> phones = new ArrayList<String>(Arrays.asList("(520) 123-4567", "(520) 111-2222"));
+        ArrayList<String> emails = new ArrayList<String>(Arrays.asList("jdoe@gmail.com", "adam.smith@yahoo.com"));
+
+        int i = 0;
+        for (Contact contact : importer)
+        {
+            assertEquals(lastNames.get(i), contact.getName().getValue().getLastName().getValue(), String.format("Verify lastNames[%d]", i));
+            assertEquals(firstNames.get(i), contact.getName().getValue().getFirstName().getValue(), String.format("Verify firstNames[%d]", i));
+            assertEquals(addresses.get(i), contact.getAddress().getValue().getStreetAddress().getValue(), String.format("Verify addresses[%d]", i));
+            assertEquals(phones.get(i), contact.getPhone().getValue(), String.format("Verify phones[%d]", i));
+            assertEquals(emails.get(i), contact.getEmail().getValue(), String.format("Verify emails[%d]", i));
+
+            i++;
+        }
+
+        assertEquals(2, i, "Verify contact count");
     }
 }
